a1c58f20b07961ef80eb82844d154c16f526feda
juancini <juani.mancini98@gmail.com>
2024-03-13 21:02:05 -0300

    moved files

---
+++ Algoritmos y Estructuras de Datos I/Laboratorio/proyecto_1/proyecto_1.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE NPlusKPatterns #-}
+
+-- # Ejercicios
+-- ## 1) programa las siguientes funciones
+-- ### a) esCero :: Int -> Bool, que verifica si un entero es igual a 0.
+
+esCero :: Int -> Bool
+esCero x = x == 0
